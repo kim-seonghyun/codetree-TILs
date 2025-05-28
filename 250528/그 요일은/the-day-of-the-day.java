b1d14b4c3b1f7eba369d8ba4ccbd1d324363a421
김성현 <60726663+kim-seonghyun@users.noreply.github.com>
2025-05-28 20:14:23 +0900

    Title: 그 요일은, Time: 146ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250528/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String A = sc.next();
+        // Please write your code here;
+        
+
+        int day = 0;
+
+        if(A.equals("Mon")){
+            day = 0;
+        }else if(A.equals("Tue")){
+            day = 1;
+        }else if(A.equals("Wed")){
+            day = 2;
+        }else if(A.equals("Thu")){
+            day = 3;
+        }else if(A.equals("Fri")){
+            day = 4;
+        }else if(A.equals("Sat")){
+            day = 5;
+        }else if(A.equals("Sun")){
+            day = 6;
+        }
+
+        int[] numOfDays = new int[]{0,31,29,31,30,31,30,31,31,30,31,30,31};
+
+        int first = 0;
+
+        for(int i=1; i< m1; i++){
+            first += numOfDays[i];
+        }
+        first += d1;
+
+        int second = 0;
+
+        for(int i=1; i< m2; i++){
+            second += numOfDays[i];
+        }
+        second += d2;
+        
+        int diffrences = second - first + 2;
+        if(diffrences - day >= 0){
+            int result = diffrences / 7;
+
+            System.out.println(result);
+        }else{
+            System.out.println(0);
+        }
+        
+    }
+}
